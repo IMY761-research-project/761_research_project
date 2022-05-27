123505f0731bdfcb393dafed38a1509293b4b9ca
devilliers_meiring <u17074292@tuks.co.za>
2022-05-27 15:21:19 +0200

    made topbar comp

--- car-infotainment-system/src/app/app.module.ts
+++ car-infotainment-system/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TopbarComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
