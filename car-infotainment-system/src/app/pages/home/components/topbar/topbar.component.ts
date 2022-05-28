6cd14a76a6b28346da741a7de0cda65fae984535
Matt Harty <matthewnharty@gmail.com>
2022-05-28 16:05:53 +0200

    Edited the tool bar

--- car-infotainment-system/src/app/pages/home/components/topbar/topbar.component.ts
+++ car-infotainment-system/src/app/pages/home/components/topbar/topbar.component.ts
@@ -4,11 +4,13 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+  styleUrls: ['./topbar.component.scss']
 })
 export class TopbarComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) { 
+    
+  }
 
   ngOnInit(): void {
   }
